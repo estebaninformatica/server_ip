a77a05d7607b30f4e2e618f07a1c7d2ab0c2e27e
Esteban Fuhrmann <esteban.informatica@gmail.com>
2015-06-07 16:04:13 -0300

    bootstrap and data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,30 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
+//= require jQuery-2.1.3.min.js
+//= require jquery-ui-1.10.3.js
+//= require bootstrap.min.js
+//= require app.js
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require plugins/slimScroll/jquery.slimscroll.min.js
+//= require plugins/datepicker/bootstrap-datepicker.js
+//= require plugins/datepicker/locales/bootstrap-datepicker.es
+//= require plugins/input-mask/jquery.inputmask.js
+//= require plugins/input-mask/jquery.inputmask.date.extensions.js
+//= require plugins/input-mask/jquery.inputmask.extensions.js
+//= require plugins/datatables/jquery.dataTables
+//= require plugins/datatables/dataTables.bootstrap
+//= require select2
+//= require select2_locale_es
+//= require notify.js
+//= require tabulous.js
+//= require deshboard2.js
+//= require_self
+
+
+
+
+
+$(".table-simple").dataTable();
